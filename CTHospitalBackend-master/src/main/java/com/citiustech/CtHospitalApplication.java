f896a4d132aee128dd7e8eeabaeb40bf08c992a7
RambhaSidar <rambhasidar01@gmail.com>
2023-02-11 13:59:39 +0530

    1st commit

---
+++ CTHospitalBackend-master/src/main/java/com/citiustech/CtHospitalApplication.java
@@ -0,0 +1,14 @@
+package com.citiustech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtHospitalApplication {
+
+	public static void main(String[] args) {
+		System.out.println("welcome");
+		SpringApplication.run(CtHospitalApplication.class, args);
+	}
+
+}
